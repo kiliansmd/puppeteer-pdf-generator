3fb734ab8221123de98b4f43f092a4147786bd00
kiliansmd <205679868+kiliansmd@users.noreply.github.com>
2025-06-12 18:22:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,5 +46,5 @@ app.get('/generate', async (req, res) => {
   res.send(pdfBuffer);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
